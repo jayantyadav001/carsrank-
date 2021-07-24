f9b6605c3b47e0aa82090c8dff7f6a5880a9058e
jayantyadav001 <74781271+jayantyadav001@users.noreply.github.com>
2021-07-24 18:47:23 +0530

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,52 @@
+class Player {
+  constructor(){
+    this.index = null;
+    this.rank = null;
+    this.distance = 0;
+    this.name = null;
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",(data)=>{
+      playerCount = data.val();
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance:this.distance
+    });
+  }
+
+  static getPlayerInfo(){
+    var playerInfoRef = database.ref('players');
+    playerInfoRef.on("value",(data)=>{
+      allPlayers = data.val();
+    })
+  }
+   getcars()
+   {
+     var dp = database.ref('carsatEnd')
+     dp.on("value",(data)=>{
+       this.rank = data.val();
+     })
+   }
+ static updateCars(rank)
+  {
+  database.ref('/').update({
+carsatEnd: rank
+  })
+  }
+
+
+}
+
